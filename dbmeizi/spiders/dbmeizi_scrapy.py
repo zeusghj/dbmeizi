46667b28a51aed2dd73c3c95d8dd81eec2a9bc0a
HongJun <zeusghj@163.com>
2016-06-22 19:02:13 +0800

    修改1

--- dbmeizi/spiders/dbmeizi_scrapy.py
+++ dbmeizi/spiders/dbmeizi_scrapy.py
@@ -1,23 +1,20 @@
 # -*- coding:utf-8 -*-
 
-# from lxml import etree
-from scrapy import Spider
-from scrapy.selector import Selector
-from dbmeizi.items import MeiziItem
-# import urllib
-# import requests
 
-class dbmeiziSpider(Spider):
+import scrapy
+from dbmeizi.items import MeiziItem
+from scrapy.selector import Selector
+
+
+class dbmeiziSpider(scrapy.Spider):
     name = "dbmeiziSpider"
-    allowed_domin = ["douban.com"]
-    start_url = [
+    allowed_domains = ["douban.com"]
+    start_urls = [
         "https://ypy.douban.com/explore/%E5%86%99%E7%9C%9F",
     ]
 
     def parse(self, response):
         liResults = Selector(response).xpath('//li/div[@class="album-item"]')
-
-        print '--------------------' + liResults + '----------------------------'
 
         for li in liResults:
             for a in li.xpath('.//a'):
@@ -34,6 +31,46 @@ class dbmeiziSpider(Spider):
                 # print '\n'
 
                 return item
+
+
+# # from lxml import etree
+# from scrapy import Spider
+# from scrapy.selector import Selector
+# from dbmeizi.items import MeiziItem
+# # import urllib
+# # import requests
+#
+# class dbmeiziSpider(Spider):
+#     name = "dbmeiziSpider"
+#     allowed_domin = ["baidu.com"]
+#     start_url = [
+#         "https://www.baidu.com/",
+#     ]
+#
+#     def parse(self, response):
+#         filename = '1234'
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+
+        # liResults = Selector(response).xpath('//li/div[@class="album-item"]')
+        #
+        # print '--------------------' + liResults + '----------------------------'
+        #
+        # for li in liResults:
+        #     for a in li.xpath('.//a'):
+        #         item = MeiziItem()
+        #         item['title'] = a.xpath('div[@class="album-title"]/text()')[0].strip()
+        #         item['dataid'] = (a.xpath('@href')[0]).split('/')[2]
+        #         item['datasrc'] = a.xpath('img/@src')[0]
+        #         item['starcount'] = 0
+        #
+        #         print item['title']
+        #         print item['datasrc']
+        #         print item['dataid']
+        #         print item['starcount']
+        #         # print '\n'
+        #
+        #         return item
 
 
 # xxx = urllib.urlopen('https://ypy.douban.com/explore/%E5%86%99%E7%9C%9F')
